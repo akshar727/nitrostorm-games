ad94bb8579debe1f17bf74e84e6d3c2629f49c6d
Elon Musk <91766805+Elon-Musk-YAY@users.noreply.github.com>
2023-05-04 21:16:18 +0000

    sad

--- nitrostormgames/core/settings.py
+++ nitrostormgames/core/settings.py
@@ -25,12 +25,13 @@ SECRET_KEY = 'django-insecure-glfpyfyc%%jqv4m5@)9tsfv)%p)*6-j23+xtgkn)(mhv!*lrs(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'frontend',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
